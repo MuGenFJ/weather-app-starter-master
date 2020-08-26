ca3f81f1a362d555c5949862cb624a4590d00c15
MuGenFJ <63530527+MuGenFJ@users.noreply.github.com>
2020-08-26 14:35:01 +0200

    Updates

---
+++ precache-manifest.c442d6269d9cbb1f3136b16117125443.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0f7c42293f7df190dd349bbfa297cfe",
+    "url": "/weather-app-starter-master/index.html"
+  },
+  {
+    "revision": "9cec1052461c3f971871",
+    "url": "/weather-app-starter-master/static/css/main.6fa17d10.chunk.css"
+  },
+  {
+    "revision": "dfcb337eac41a213b50d",
+    "url": "/weather-app-starter-master/static/js/2.5d4c6e57.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app-starter-master/static/js/2.5d4c6e57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cec1052461c3f971871",
+    "url": "/weather-app-starter-master/static/js/main.4fcbe2d4.chunk.js"
+  },
+  {
+    "revision": "6e17c09840ff436859e2",
+    "url": "/weather-app-starter-master/static/js/runtime-main.89f1b99f.js"
+  }
+]);
